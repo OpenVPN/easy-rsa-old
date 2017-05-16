7f9f95ea0daefc4d537b59158d08ea2cf1f76e20
Samuli Seppänen <samuli@openvpn.net>
2017-05-16 15:01:26 +0300

    Allow use of paths with spaces on Windows
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- easy-rsa/Windows/build-dh.bat
+++ easy-rsa/Windows/build-dh.bat
@@ -1,4 +1,4 @@
 @echo off
-cd %HOME%
+cd "%HOME%"
 rem build a dh file for the server side
-openssl dhparam -out %KEY_DIR%/dh%KEY_SIZE%.pem %KEY_SIZE%
+openssl dhparam -out "%KEY_DIR%/dh%KEY_SIZE%.pem" %KEY_SIZE%
