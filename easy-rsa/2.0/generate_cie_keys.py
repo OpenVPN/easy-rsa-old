9947a205aec046c9cba23b05cf7e9ebbf2c75b69
Bala Chandrasekaran <bala@pixglo.com>
2018-02-14 12:23:39 +0530

    Key generation for CIE.
    This script generates 1000 keys

---
+++ easy-rsa/2.0/generate_cie_keys.py
@@ -0,0 +1,8 @@
+import subprocess
+
+count = 1000
+
+for index in range(1, count+1):
+    process = subprocess.Popen("/bin/bash", stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True, encoding='utf8')
+    proc_stdout = process.communicate(input = "source ./vars; ./build-key --batch cie%d" %(index))
+    print(proc_stdout)
